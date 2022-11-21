464c5f25265c2b6d240f5d95ac0024e31bd61dbf
Phelan164 <phelan.nguyen@instill.tech>
2022-11-21 15:15:57 +0700

    chore: create install, uninstall helm chart script

--- charts/script/uninstall.sh
+++ charts/script/uninstall.sh
@@ -5,3 +5,4 @@ INSTILL_SERVICES=$1
 helm uninstall -n triton-backend triton-backend helm-triton-backend/triton-backend  > /dev/null 2>&1 || true
 for srv in ${INSTILL_SERVICES}; do helm uninstall -n $srv $srv helm-$srv/$srv > /dev/null 2>&1 || true ; done
 kill -9 $(lsof -t -i:8000) > /dev/null 2>&1 || true
+kill -9 $(lsof -t -i:3000) > /dev/null 2>&1 || true
