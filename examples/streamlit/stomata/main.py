946830a8c49d9355b83843795e4cff0f9b4c7c1a
Xiaofei Du <xiaofei.du@instill.tech>
2023-02-14 15:14:05 +0000

    chore: update default pipeline backend base url

--- examples/streamlit/stomata/main.py
+++ examples/streamlit/stomata/main.py
@@ -128,7 +128,7 @@ def display_trigger_request_code(pipeline_id, filename):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--pipeline-backend-base-url', type=str,
-                        default='http://localhost:8081', help='pipeline backend base URL')
+                        default='http://localhost:8080', help='pipeline backend base URL')
     parser.add_argument('--pipeline-id', type=str,
                         default='stomata', help='Stomata instance segmentation pipeline ID on VDP')
     opt = parser.parse_args()
